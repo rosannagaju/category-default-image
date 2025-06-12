fedb9c1bee4d066568dece5956ebbf7d65c76ed2
Roshan Nagaju <rosannagaju@gmail.com>
2025-06-12 13:17:58 +0545

    Text domain update

--- category-default-image.php
+++ category-default-image.php
@@ -9,7 +9,7 @@
  * Author:            Roshan Nagaju
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       wc-google-map
+ * Text Domain:       category-default-image
  */
 
 // Exit if accessed directly
